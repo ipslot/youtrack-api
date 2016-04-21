95b8bb3877af3e09baae6af2169ceefb5f13b489
Vacheslav K <kenstavichus@gmail.com>
2016-04-21 11:38:38 +0700

    initial

---
+++ client.py
@@ -0,0 +1,103 @@
+# vim: set fileencoding=utf-8
+from __future__ import unicode_literals
+
+import time
+import json
+import http
+import http.client
+import urllib.parse
+import urllib.request
+from base64 import b64encode
+
+from parser import extract_task_list, extract_spent_time_list
+
+ISSUE_URI = "/rest/issue"
+TIME_TRACKING_URI = ISSUE_URI + "/{issue_id}/timetracking/workitem"
+
+
+class YTClient:
+
+    def __init__(self, uid, secret, scope, api_host, api_port=443,
+                 auth_host=None, auth_port=None):
+        self._uid = uid
+        self._secret = secret
+        self._scope = scope
+        self._api_host = api_host
+        self._api_port = api_port
+        self._auth_key = None
+        self._auth_key_expired = 0
+        self._auth_host = auth_host if auth_host else api_host
+        self._auth_port = auth_port if auth_port else api_port
+
+    def is_authenticated(self):
+        timestamp = int(time.time())
+        return self._auth_key and self._auth_key_expired > timestamp
+
+    def auth(self):
+        if not self.is_authenticated():
+            auth_host = self._auth_host.split("/")[0]
+            auth_uri = (self._auth_host.replace(auth_host, "/") + "/").\
+                replace("//", "/")
+            conn = http.client.HTTPSConnection(
+                host=auth_host,
+                port=self._auth_port)
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization":
+                    "{type} {token}".format(
+                        type="Basic",
+                        token=b64encode(
+                            "{uid}:{secret}".
+                            format(uid=self._uid, secret=self._secret).
+                            encode("ascii")).
+                        decode("utf8"))}
+            params = urllib.parse.urlencode({
+                "grant_type": "client_credentials",
+                "scope": self._scope})
+            conn.request("POST",
+                         auth_uri + "api/rest/oauth2/token",
+                         params,
+                         headers=headers)
+            resp = conn.getresponse()
+            if resp.status == 200:
+                data = json.loads(resp.read().decode("utf8"))
+                self._auth_key = data.get("access_token", None)
+                self._auth_key_expired = \
+                    int(time.time()) + int(data.get("expires_in", 0))
+        return self._auth_key
+
+    def make_basic_request(self):
+        conn = http.client.HTTPSConnection(
+            host=self._api_host,
+            port=self._api_port)
+        headers = {"Authorization": "{type} {token}".format(
+            type="Bearer",
+            token=self.auth())}
+        return conn, headers
+
+    def get_flagged_tasks(self, flag_name):
+        return self.get_queried_tasks("tag:{flag}".format(flag=flag_name))
+
+    def get_queried_tasks(self, query):
+        conn, headers = self.make_basic_request()
+        conn.request(
+            "GET",
+            ISSUE_URI + "?filter=" + urllib.request.quote(query),
+            headers=headers)
+        resp = conn.getresponse()
+        if resp.status == 200:
+            return extract_task_list(resp.read())
+        else:
+            return None
+
+    def get_spent_time_for_task(self, task_id):
+        conn, headers = self.make_basic_request()
+        conn.request(
+            "GET",
+            TIME_TRACKING_URI.format(issue_id=task_id),
+            headers=headers)
+        resp = conn.getresponse()
+        if resp.status == 200:
+            return extract_spent_time_list(resp.read())
+        else:
+            return None
---
+++ parser.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import xml.etree.ElementTree
+
+TYPE_FIELD_NAME = '{http://www.w3.org/2001/XMLSchema-instance}type'
+
+
+def extract_field(name, xml_field, task):
+    if xml_field.get('name') == name:
+        result = xml_field.findall('value')
+        if xml_field.get(TYPE_FIELD_NAME) == 'SingleField' or \
+                        xml_field.get(TYPE_FIELD_NAME) == 'CustomFieldValue':
+            if len(result) > 1:
+                task[name] = [item.text for item in result]
+            else:
+                task[name] = xml_field.findall('value')[0].text
+            return True
+        elif xml_field.get(TYPE_FIELD_NAME) == 'LinkField':
+            task[name] = [
+                {
+                    "value": item.text,
+                    "type": item.get('type'),
+                    "role": item.get('role')
+                }
+                for item in result
+            ]
+    return False
+
+
+def extract_task(xml_task):
+    result = {"id": xml_task.get('id'), "tags": []}
+    # extract tags
+    for tag in xml_task.findall('tag'):
+        result['tags'].append(tag.text)
+    # extract fields
+    for field in xml_task.findall('field'):
+        if extract_field("summary", field, result):
+            continue
+        if extract_field("projectShortName", field, result):
+            continue
+        if extract_field("numberInProject", field, result):
+            continue
+        if extract_field("description", field, result):
+            continue
+        if extract_field("created", field, result):
+            continue
+        if extract_field("updated", field, result):
+            continue
+        if extract_field("links", field, result):
+            continue
+        if extract_field("State", field, result):
+            continue
+        if extract_field("Priority", field, result):
+            continue
+        if extract_field("Type", field, result):
+            continue
+        if extract_field("Assignee", field, result):
+            continue
+        if extract_field("Spent time", field, result):
+            continue
+        if extract_field("Estimation", field, result):
+            continue
+    return result
+
+
+def extract_task_list(string_xml_list):
+    root = xml.etree.ElementTree.fromstring(string_xml_list)
+    if not len(root):
+        return []
+    return [extract_task(child) for child in root.findall('issue')]
+
+
+def extract_spent_time(xml_time_entry):
+    w_type = xml_time_entry.find('worktype')
+    desc = xml_time_entry.find('description')
+    return {
+        "id": xml_time_entry.find('id').text,
+        "date": xml_time_entry.find('date').text,
+        "duration": xml_time_entry.find('duration').text,
+        "description": desc.text if desc is not None else None,
+        "user": xml_time_entry.find('author').get("login"),
+        "workType": w_type.find('name').text if w_type is not None else None
+    }
+
+
+def extract_spent_time_list(string_xml_list):
+    root = xml.etree.ElementTree.fromstring(string_xml_list)
+    if not len(root):
+        return []
+    return [extract_spent_time(child) for child in root.findall('workItem')]
